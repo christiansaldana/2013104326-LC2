dd06f82ba289a48699914e6575ccaf1e5d7ac155
christiansaldana <christian_saldana@usmp.pe>
2017-05-14 23:40:57 -0500

    NewLineAllClass2

---
+++ NewLine/NewLine.Entities/IRepositories/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLine.Entities.IRepositories
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+
+       
+        void add(TEntity entity);        
+        void addRange(IEnumerable<TEntity> entities);
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerator<TEntity> Find(Expression<Func<TEntity, bool>> predicate);             
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);       
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+    }
+}
---
+++ NewLine/NewLine.Entities/IRepositories/IUnityOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLine.Entities.IRepositories
+{
+    public interface IUnityOfWork : IDisposable
+    {
+        IAdministradorEquipoRepository AdministradorEquipos { get; }
+        IAdministradorLineaRepository AdministradorLineas { get; }
+        ICentroAtencionRepository CentroAtencions { get; }
+        IClienteRepository Clientes { get; }
+        IContratoRepository Contratos { get; }
+        IDepartamentoRepository Departamentos { get; }
+        IDireccionRepository Direccions { get; }
+        IDistritoRepository Distritos { get; }
+        IEquipoCelularRepository EquipoCelulars { get; }
+        IEstadoEvaluacionRepository EstadoEvaluacions { get; }
+        IEvaluacionRepository Evaluacions { get; }
+        ILineaTelefonicaRepository LineaTelefonicas { get; }
+        IPlanRepository Plans { get; }
+        IProvinciaRepository Provincias { get; }
+        ITipoEvaluacionRepository TipoEvaluacions { get; }
+        ITipoLineaRepository TipoLineas { get; }
+        ITipoPagoRepository TipoPagos { get; }
+        ITipoPlanRepository TipoPlans { get; }
+        ITipoTrabajadorRepository TipoTrabajadors { get; }
+        ITrabajadorRepository Trabajadors { get; }
+        IUbigeoRepository Ubigeos { get; }
+        IVentaRepository Ventas { get; }
+        int SaveChanges();
+    }
+}
